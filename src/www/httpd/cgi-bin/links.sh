93c91fa22662ed9956561a7528c8e0d25b67736e
roleo <roleo@iperbole.bologna.it>
2020-08-20 15:33:22 +0200

    Fix username/password for onvif and rtsp services

--- src/www/httpd/cgi-bin/links.sh
+++ src/www/httpd/cgi-bin/links.sh
@@ -26,14 +26,18 @@ if [[ $HTTPD_PORT != "80" ]] ; then
     D_HTTPD_PORT=:$HTTPD_PORT
 fi
 
-RTSP_USER="rtsp"
-RTSP_PWD=$(/mnt/mmc/sonoff-hack/bin/sqlite3 /mnt/mtd/db/ipcsys.db "select c_PassWord from t_user where c_UserName='$RTSP_USER';")
+RTSP_USER=$(/mnt/mmc/sonoff-hack/bin/sqlite3 /mnt/mtd/db/ipcsys.db "select c_UserName from t_user where C_UserID=10101;")
+RTSP_PWD=$(/mnt/mmc/sonoff-hack/bin/sqlite3 /mnt/mtd/db/ipcsys.db "select c_PassWord from t_user where C_UserID=10101;")
+
+if [ x$RTSP_USER == "xhack" ] && [ x$RTSP_PWD == "xhack" ]; then
+    RTSP_USERPWD="hack:hack@"
+fi
 
 printf "Content-type: application/json\r\n\r\n"
 printf "{\n"
 
-printf "\"%s\":\"%s\",\n" "low_res_stream"        "rtsp://$RTSP_USER:$RTSP_PWD@$LOCAL_IP$D_RTSP_PORT/av_stream/ch1"
-printf "\"%s\":\"%s\",\n" "high_res_stream"       "rtsp://$RTSP_USER:$RTSP_PWD@$LOCAL_IP$D_RTSP_PORT/av_stream/ch0"
+printf "\"%s\":\"%s\",\n" "low_res_stream"        "rtsp://$RTSP_USERPWD$LOCAL_IP$D_RTSP_PORT/av_stream/ch1"
+printf "\"%s\":\"%s\",\n" "high_res_stream"       "rtsp://$RTSP_USERPWD$LOCAL_IP$D_RTSP_PORT/av_stream/ch0"
 
 printf "\"%s\":\"%s\"\n" "high_res_snapshot"      "http://$LOCAL_IP$D_HTTPD_PORT/cgi-bin/snapshot.sh"
 
--- src/www/httpd/cgi-bin/set_configs.sh
+++ src/www/httpd/cgi-bin/set_configs.sh
@@ -34,6 +34,8 @@ fi
 read POST_DATA
 
 PARAMS=$(echo "$POST_DATA" | tr "\n\r" " " | tr -d " " | sed 's/{\"//g' | sed 's/\"}//g' | sed 's/\",\"/ /g' | sed 's/\":\"/=/g')
+USERNAME="none"
+PASSWORD="none"
 
 for S in $PARAMS ; do
     PARAM=$(echo "$S" | tr "=" " ")
@@ -67,9 +69,31 @@ for S in $PARAMS ; do
     else
         VALUE=$(echo "$VALUE" | sedencode)
         sed -i "s/^\(${KEY}\s*=\s*\).*$/\1${VALUE}/" $CONF_FILE
+
+        if [ "$KEY" == "USERNAME" ] ; then
+            USERNAME=$VALUE
+        elif [ "$KEY" == "PASSWORD" ] ; then
+            PASSWORD=$VALUE
+        fi
     fi
 
 done
+
+if [ "$CONF_TYPE" == "system" ] && [ x$USERNAME != "none" ]; then
+    # Add username and password to t_user table
+    # If we don't add user now we need 2 reboots
+    SQL_USER=$(sqlite3 /mnt/mtd/db/ipcsys.db "select C_UserName from t_user where C_UserID=10101;")
+    SQL_PWD=$(sqlite3 /mnt/mtd/db/ipcsys.db "select C_PassWord from t_user where C_UserID=10101;")
+
+    if [ x$SQL_USER != x$USERNAME ] || [ x$SQL_PWD != x$PASSWORD ]; then
+        sqlite3 /mnt/mtd/db/ipcsys.db "delete from t_user where C_UserID=10101;"
+        if [[ x$USERNAME != "x" ]] ; then
+            sqlite3 /mnt/mtd/db/ipcsys.db "insert into t_user (C_UserID, c_role_id, C_UserName, C_PassWord) values (10101, 1, '$USERNAME', '$PASSWORD');"
+        else
+            sqlite3 /mnt/mtd/db/ipcsys.db "insert into t_user (C_UserID, c_role_id, C_UserName, C_PassWord) values (10101, 1, 'hack', 'hack');"
+        fi
+    fi
+fi
 
 # Yeah, it's pretty ugly.
 

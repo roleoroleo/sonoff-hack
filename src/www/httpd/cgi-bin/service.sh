be515263c310c841d3e0cf2ca70d66ff86ea3fd5
roleo <roleo@iperbole.bologna.it>
2020-08-20 17:05:56 +0200

    Fix web service for process start/stop

--- src/www/httpd/cgi-bin/service.sh
+++ src/www/httpd/cgi-bin/service.sh
@@ -18,13 +18,13 @@ init_config()
     if [[ x$(get_config USERNAME) != "x" ]] ; then
         USERNAME=$(get_config USERNAME)
         PASSWORD=$(get_config PASSWORD)
+        RTSP_USERPWD=""
         ONVIF_USERPWD="--user $USERNAME --password $PASSWORD"
+        echo "/:$USERNAME:$PASSWORD" > /tmp/httpd.conf
+    else
+        RTSP_USERPWD="hack:hack@"
     fi
 
-    case $(get_config RTSP_PORT) in
-        ''|*[!0-9]*) RTSP_PORT=554 ;;
-        *) RTSP_PORT=$(get_config RTSP_PORT) ;;
-    esac
     case $(get_config ONVIF_PORT) in
         ''|*[!0-9]*) ONVIF_PORT=80 ;;
         *) ONVIF_PORT=$(get_config ONVIF_PORT) ;;
@@ -33,10 +33,6 @@ init_config()
         ''|*[!0-9]*) HTTPD_PORT=8080 ;;
         *) HTTPD_PORT=$(get_config HTTPD_PORT) ;;
     esac
-
-    if [[ $RTSP_PORT != "554" ]] ; then
-        D_RTSP_PORT=:$RTSP_PORT
-    fi
 
     if [[ $ONVIF_PORT != "80" ]] ; then
         D_ONVIF_PORT=:$ONVIF_PORT
@@ -51,27 +47,22 @@ init_config()
     else
         ONVIF_NETIF="eth0"
     fi
-
-    RTSP_USER="rtsp"
-    RTSP_PWD=$(/mnt/mmc/sonoff-hack/bin/sqlite3 /mnt/mtd/db/ipcsys.db "select c_PassWord from t_user where c_UserName='$RTSP_USER';")
 }
 
 start_onvif()
 {
     if [[ $1 == "high" ]]; then
-        ONVIF_PROFILE_0="--name Profile_0 --width 1920 --height 1080 --url rtsp://$RTSP_USER:$RTSP_PWD@%s$D_RTSP_PORT/av_stream/ch0 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh --type H264"
+        ONVIF_PROFILE_0="--name Profile_0 --width 1920 --height 1080 --url rtsp://$RTSP_USERPWD%s/av_stream/ch0 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh --type H264"
     fi
     if [[ $1 == "low" ]]; then
-        ONVIF_PROFILE_1="--name Profile_1 --width 640 --height 360 --url rtsp://$RTSP_USER:$RTSP_PWD@%s$D_RTSP_PORT/av_stream/ch1 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh --type H264"
+        ONVIF_PROFILE_1="--name Profile_1 --width 640 --height 360 --url rtsp://$RTSP_USERPWD%s/av_stream/ch1 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh --type H264"
     fi
     if [[ $1 == "both" ]]; then
-        ONVIF_PROFILE_0="--name Profile_0 --width 1920 --height 1080 --url rtsp://$RTSP_USER:$RTSP_PWD@%s$D_RTSP_PORT/av_stream/ch0 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh --type H264"
-        ONVIF_PROFILE_1="--name Profile_1 --width 640 --height 360 --url rtsp://$RTSP_USER:$RTSP_PWD@%s$D_RTSP_PORT/av_stream/ch1 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh --type H264"
+        ONVIF_PROFILE_0="--name Profile_0 --width 1920 --height 1080 --url rtsp://$RTSP_USERPWD%s/av_stream/ch0 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh --type H264"
+        ONVIF_PROFILE_1="--name Profile_1 --width 640 --height 360 --url rtsp://$RTSP_USERPWD%s/av_stream/ch1 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh --type H264"
     fi
 
-    onvif_srvd --pid_file /var/run/onvif_srvd.pid --model "Sonoff Hack" --manufacturer "Sonoff" --firmware_ver "$SONOFF_HACK_VER" --hardware_id $MODEL --serial_num $DEVICE_ID --ifs $ONVIF_NETIF --port $ONVIF_PORT --scope onvif://www.onvif.org/Profile/S $ONVIF_PROFILE_0 $ONVIF_PROFILE_1 $ONVIF_USERPWD --ptz --move_left "/home/yi-hack/bin/ptz -a left" --move_right "/home/yi-hack/bin/ptz -a right" --move_up "/home/yi-hack/bin/ptz -a up" --move_down "/home/yi-hack/bin/ptz -a down" --move_stop "/home/yi-hack/bin/ptz -a stop"
-# --move_preset "/home/yi-hack/bin/ipc_cmd -p"
-    wsdd --pid_file /var/run/wsdd.pid --if_name $ONVIF_NETIF --type tdn:NetworkVideoTransmitter --xaddr http://%s$D_ONVIF_PORT --scope "onvif://www.onvif.org/name/Unknown onvif://www.onvif.org/Profile/Streaming"
+    onvif_srvd --pid_file /var/run/onvif_srvd.pid --model "Sonoff Hack" --manufacturer "Sonoff" --firmware_ver "$SONOFF_HACK_VER" --hardware_id $MODEL --serial_num $DEVICE_ID --ifs $ONVIF_NETIF --port $ONVIF_PORT --scope onvif://www.onvif.org/Profile/S $ONVIF_PROFILE_0 $ONVIF_PROFILE_1 $ONVIF_USERPWD --ptz --move_left "/mnt/mmc/sonoff-hack/bin/ptz -a left" --move_right "/mnt/mmc/sonoff-hack/bin/ptz -a right" --move_up "/mnt/mmc/sonoff-hack/bin/ptz -a up" --move_down "/mnt/mmc/sonoff-hack/bin/ptz -a down" --move_stop "/mnt/mmc/sonoff-hack/bin/ptz -a stop" --move_preset "/mnt/mmc/sonoff-hack/bin/ptz -f /mnt/mmc/sonoff-hack/etc/ptz_presets.conf -a go_preset -n"
 }
 
 stop_onvif()
@@ -110,7 +101,6 @@ stop_ftpd()
 NAME="none"
 ACTION="none"
 PARAM1="none"
-PARAM2="none"
 
 for I in 1 2 3
 do
@@ -133,7 +123,7 @@ if [ "$ACTION" == "start" ] ; then
         start_onvif $PARAM1
     elif [ "$NAME" == "wsdd" ]; then
         start_wsdd
-	elif [ "$NAME" == "ftpd" ]; then
+    elif [ "$NAME" == "ftpd" ]; then
         start_ftpd $PARAM1
     elif [ "$NAME" == "mqtt" ]; then
         mqttv4 >/dev/null &

32ab60857aeedded591d5f3f0670fe311fa9ace1
roleo <roleo@iperbole.bologna.it>
2020-09-10 14:04:14 +0200

    Fix getlastrecordedvideo web service

--- src/www/httpd/cgi-bin/getlastrecordedvideo.sh
+++ src/www/httpd/cgi-bin/getlastrecordedvideo.sh
@@ -4,15 +4,14 @@
 # "oldness" variable. Defines which video to retrieve. 
 # - "0" (default) or "1" means latest already available.
 # - greater than "0" specifies the oldness, so "3" means the third oldest video. Only looks into the current hour folder. So if value is greater than the videos in the latest hour folder, the last one will be sent.
-# - negative value (e.g. "-1") means to wait for the next available video. Only works if a video is already being recorded, max. time to wait 80 seconds.
 # "type" variable. Defines what to retrieve.
 # - "1" (default). Gets relative name in the format of DIR/VIDEO.mp4
 # - "2". Gets full URL video.
 # - "3". Gets the video itself as video/mp4 inline.
 # - "4". Gets the video itself as video/mp4 attachment.
 # Examples of use:
-# http://IP:PORT/cgi-bin/getlastrecordedvideo.sh?oldness=-1&type=4 -- Wait for a new video and sends it as an attachment.
-# http://IP:PORT/cgi-bin/getlastrecordedvideo.sh?oldness=-1&type=3 -- Wait for a new video and shows it inline in the browser.
+# http://IP:PORT/cgi-bin/getlastrecordedvideo.sh?oldness=0&type=4 -- Send the last video as an attachment.
+# http://IP:PORT/cgi-bin/getlastrecordedvideo.sh?oldness=0&type=3 -- Show the last video inline in the browser.
 # http://IP:PORT/cgi-bin/getlastrecordedvideo.sh?type=1            -- Send the relative route of the last available video.
 # http://IP:PORT/cgi-bin/getlastrecordedvideo.sh?oldness=2&type=2  -- Send the URL of the second to last available video.
 
@@ -25,7 +24,7 @@ do
     CONF="$(echo $QUERY_STRING | cut -d'&' -f$I | cut -d'=' -f1)"
     VAL="$(echo $QUERY_STRING | cut -d'&' -f$I | cut -d'=' -f2)"
 
-    if [ $CONF == $CONF_LAST ]; then
+    if [ "$CONF" == "$CONF_LAST" ]; then
         continue
     fi
     CONF_LAST=$CONF
@@ -37,53 +36,11 @@ do
     fi
 done
 
-
-
-if [ "$OLDNESS" -lt "0" ]; then
-
-    # This section just wait until a new file comes or we timeout. After that, the next section will give the last modified file.
-    # If oldness is negative, try to wait until a new file comes.
-    if [ -f "/tmp/sd/record/tmp.mp4.tmp" ]; then
-        # A file is being recorded, therefore, try to get it when it finished saving.
-        # First, we get the last directory.
-        for f in `ls -At /tmp/sd/record | grep H`; do
-            if [ ${#f} == 14 ]; then
-                DIRNAME="$f"
-                break;
-            fi
-        done
-
-        # Now we get the number of directories and files in the last modified directory.
-        FILECOUNT=`ls -At /tmp/sd/record/$DIRNAME | grep .mp4 -c`
-        DIRCOUNT=`ls -At /tmp/sd/record/ | grep H -c`
-        SLEEPCOUNT=0
-        while [ "$FILECOUNT" -eq `ls -At /tmp/sd/record/$DIRNAME | grep .mp4 -c` ]; do
-            if [ "$SLEEPCOUNT" -gt 800 ]; then
-                # After 80 seconds, we break the wait.
-                break;
-            elif [ "$DIRCOUNT" -lt `ls -At /tmp/sd/record/ | grep H -c` ]; then
-                # If a new dir comes, we break the wait.
-                break;
-            fi
-            sleep 0.1
-            SLEEPCOUNT=$(($SLEEPCOUNT+1))
-        done
-    fi
-fi
-
-
-for f in `ls -At /tmp/sd/record | grep H`; do
-    if [ ${#f} == 14 ]; then
-        DIRNAME="$f"
-        break;
-    fi
-done
-# In $DIRNAME we now has the last modified directory.
-
-COUNT=`ls -At /tmp/sd/record/$DIRNAME | grep .mp4 -c`
+L_FILE_LIST=`find /mnt/mmc/alarm_record -mindepth 1 -type f \( -name "*.mp4" \) | sort -k 1 -n -r | sed 's/\/mnt\/mmc\/alarm_record\///g'`
+COUNT=`find /mnt/mmc/alarm_record -mindepth 1 -type f \( -name "*.mp4" \) | grep ^ -c`
 IDX=1
-for f in `ls -At /tmp/sd/record/$DIRNAME`; do
-    if [ ${#f} == 12 ]; then
+for f in $L_FILE_LIST; do
+    if [ ${#f} == 33 ]; then
         VIDNAME="$f"
         if [ "$IDX" -ge "$OLDNESS" ]; then
             break;
@@ -95,22 +52,25 @@ for f in `ls -At /tmp/sd/record/$DIRNAME`; do
 done
 
 if [ "$TYPE" == "2" ]; then
-    LOCAL_IP=$(ifconfig wlan0 | awk '/inet addr/{print substr($2,6)}')
-    source /home/yi-hack/etc/system.conf
+    LOCAL_IP=$(ifconfig eth0 | awk '/inet addr/{print substr($2,6)}')
+    if [ -z $LOCAL_IP ]; then
+        LOCAL_IP=$(ifconfig ra0 | awk '/inet addr/{print substr($2,6)}')
+    fi
+    source /mnt/mmc/sonoff-hack/etc/system.conf
 
     printf "Content-type: text/plain\r\n\r\n"
-    echo "http://$LOCAL_IP:$HTTPD_PORT/record/$DIRNAME/$VIDNAME"
+    echo "http://$LOCAL_IP:$HTTPD_PORT/alarm_record/$VIDNAME"
 elif [ "$TYPE" == "3" ]; then
     printf "Content-type: video/mp4; charset=utf-8\r\nContent-Disposition: inline; filename=\"$VIDNAME\"\r\n\r\n"
-    cat /tmp/sd/record/$DIRNAME/$VIDNAME
+    cat /mnt/mmc/alarm_record/$VIDNAME
     exit
 elif [ "$TYPE" == "4" ]; then
     printf "Content-type: video/mp4; charset=utf-8\r\nContent-Disposition: attachment; filename=\"$VIDNAME\"\r\n\r\n"
-    cat /tmp/sd/record/$DIRNAME/$VIDNAME
+    cat /mnt/mmc/alarm_record/$VIDNAME
     exit
 else
     # Default and type=1
     printf "Content-type: text/plain\r\n\r\n"
-    echo "$DIRNAME/$VIDNAME"
+    echo "$VIDNAME"
     exit
 fi

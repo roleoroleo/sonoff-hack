2d51713eb6b9cf8d5bad48c7260baabd44360bd2
roleo <roleo@iperbole.bologna.it>
2020-12-04 11:34:45 +0100

    Add crontab feature

--- src/www/httpd/cgi-bin/set_configs.sh
+++ src/www/httpd/cgi-bin/set_configs.sh
@@ -2,14 +2,13 @@
 
 SONOFF_HACK_PREFIX="/mnt/mmc/sonoff-hack"
 
-#urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
-
-urldecode(){
-  echo -e "$(sed 's/+/ /g;s/%\(..\)/\\x\1/g;')"
+sedencode(){
+#  echo -e "$(sed 's/\\/\\\\\\/g;s/\&/\\\&/g;s/\//\\\//g;')"
+  echo "$(sed 's/\\/\\\\/g;s/\&/\\\&/g;s/\//\\\//g;')"
 }
 
-sedencode(){
-  echo -e "$(sed 's/\\/\\\\\\/g;s/\&/\\\&/g;s/\//\\\//g;')"
+removedoublequotes(){
+  echo "$(sed 's/^"//g;s/"$//g')"
 }
 
 get_conf_type()
@@ -31,25 +30,16 @@ else
     CONF_FILE="$SONOFF_HACK_PREFIX/etc/$CONF_TYPE.conf"
 fi
 
-read POST_DATA
-
-PARAMS=$(echo "$POST_DATA" | tr "\n\r" " " | tr -d " " | sed 's/{\"//g' | sed 's/\"}//g' | sed 's/\",\"/ /g')
 USERNAME="none"
 PASSWORD="none"
 TIMEZONE="none"
 
-for S in $PARAMS ; do
-    PARAM=$(echo "$S" | sed 's/\":\"/ /g')
-    KEY=""
-    VALUE=""
+read -r POST_DATA
 
-    for SP in $PARAM ; do
-        if [ -z $KEY ]; then
-            KEY=$SP
-        else
-            VALUE=$SP
-        fi
-    done
+KEYS=$(echo "$POST_DATA" | jq keys_unsorted[])
+for KEY in $KEYS; do
+    KEY=$(echo $KEY | removedoublequotes)
+    VALUE=$(echo "$POST_DATA" | jq .$KEY | removedoublequotes)
 
     if [ "$KEY" == "HOSTNAME" ] ; then
         if [ -z $VALUE ] ; then

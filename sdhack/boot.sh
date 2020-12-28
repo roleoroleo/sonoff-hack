6cb95d8b66fa3766c8552b19d842e66381cb5544
roleo <roleo@iperbole.bologna.it>
2020-12-28 14:24:16 +0100

    Fix hostname in udhcpc

--- sdhack/boot.sh
+++ sdhack/boot.sh
@@ -10,4 +10,9 @@ mount --bind /tmp/di.wav /mnt/mtd/ipc/app/res/En/di.wav
 mount --bind /tmp/Internet_connected_Welcome_to_use_cloud_camera.wav /mnt/mtd/ipc/app/res/En/Internet_connected_Welcome_to_use_cloud_camera.wav
 mount --bind /tmp/WiFi_connect_success.wav /mnt/mtd/ipc/app/res/En/WiFi_connect_success.wav
 
+cp /mnt/mtd/ipc/app/script/dhcp.sh /tmp/dhcp.sh
+HOSTNAME=$(cat /var/sdcard/sonoff-hack/etc/hostname)
+sed -i "s/dhcpname=\$2/dhcpname=$HOSTNAME/g" /tmp/dhcp.sh
+mount --bind /tmp/dhcp.sh /mnt/mtd/ipc/app/script/dhcp.sh
+
 (sleep 30 && /mnt/mmc/sonoff-hack/script/system.sh) &

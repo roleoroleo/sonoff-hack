5efe8fdb7d76bd277c77054b8acf3a8d86034ceb
Dmitriy Shafranskiy <dmitriy.shafranskiy.git@gmail.com>
2021-06-22 15:12:30 +0200

    Create compile_ci_post.sh

---
+++ scripts/compile_ci_post.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+#  This file is part of sonoff-hack.
+#  Copyright (c) 2018-2019 Davide Maggioni.
+#  Copyright (c) 2020 roleo.
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3.
+# 
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+get_script_dir()
+{
+    echo "$(cd `dirname $0` && pwd)"
+}
+
+BIN_DIR=$(get_script_dir)/../bin
+BUILD_DIR=$(get_script_dir)/../build
+
+if [ -d "$BIN_DIR" ]; then
+    cp -R $BIN_DIR/* $BUILD_DIR/
+fi

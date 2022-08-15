a8424f584e5fb32d5a0d9aab629a858dc5501d67
puuu <puuu@users.noreply.github.com>
2022-08-15 11:03:59 +0900

    Fix position of wav files

--- sdhack/boot.sh
+++ sdhack/boot.sh
@@ -7,9 +7,12 @@ touch /tmp/di.wav
 touch /tmp/Internet_connected_Welcome_to_use_cloud_camera.wav
 touch /tmp/WiFi_connect_success.wav
 
-mount --bind /tmp/di.wav /mnt/mtd/ipc/app/res/En/di.wav
-mount --bind /tmp/Internet_connected_Welcome_to_use_cloud_camera.wav /mnt/mtd/ipc/app/res/En/Internet_connected_Welcome_to_use_cloud_camera.wav
-mount --bind /tmp/WiFi_connect_success.wav /mnt/mtd/ipc/app/res/En/WiFi_connect_success.wav
+WAV_FILE_DIR=/mnt/mtd/ipc/app/res/En
+[ -d /mnt/mtd/ipc/app/snd/english ] && WAV_FILE_DIR=/mnt/mtd/ipc/app/snd/english
+
+mount --bind /tmp/di.wav $WAV_FILE_DIR/di.wav
+mount --bind /tmp/Internet_connected_Welcome_to_use_cloud_camera.wav $WAV_FILE_DIR/Internet_connected_Welcome_to_use_cloud_camera.wav
+mount --bind /tmp/WiFi_connect_success.wav $WAV_FILE_DIR/WiFi_connect_success.wav
 
 # Fix hostname
 cp /mnt/mtd/ipc/app/script/dhcp.sh /tmp/dhcp.sh

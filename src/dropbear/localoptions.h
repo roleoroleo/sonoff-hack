1f9e3c82d090a1957a9a0d7f669a7544bf2a1a4d
puuu <puuu@users.noreply.github.com>
2022-08-19 12:17:27 +0900

    Let dropberar use sftp-server

--- src/dropbear/localoptions.h
+++ src/dropbear/localoptions.h
@@ -10,4 +10,7 @@
 
 #define DEFAULT_PATH "/usr/bin:/usr/sbin:/bin:/sbin:/gm/bin:/gm/tools:/mnt/mmc/sonoff-hack/bin:/mnt/mmc/sonoff-hack/sbin:/mnt/mmc/sonoff-hack/usr/bin:/mnt/mmc/sonoff-hack/usr/sbin"
 
+#define SFTPSERVER_PATH "/mnt/mmc/sonoff-hack/usr/libexec/sftp-server"
+
+
 #endif /* DROPBEAR_LOCALOPTIONS_H */

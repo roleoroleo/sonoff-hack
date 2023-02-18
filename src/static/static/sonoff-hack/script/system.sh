7ea74654f59f7ee2aff46047a294954f8f5a8d9b
roleo <roleo@iperbole.bologna.it>
2023-02-18 14:55:22 +0100

    Add option to disable thumbnails

--- src/static/static/sonoff-hack/script/system.sh
+++ src/static/static/sonoff-hack/script/system.sh
@@ -340,7 +340,9 @@ mkdir -p /var/spool/cron/crontabs/
 if [ ! -z "$CRONTAB" ]; then
     echo -e "$CRONTAB" > /var/spool/cron/crontabs/root
 fi
-echo "* * * * * /mnt/mmc/sonoff-hack/script/thumb.sh cron" >> /var/spool/cron/crontabs/root
+if [[ $(get_config SNAPSHOT_VIDEO) == "yes" ]] ; then
+    echo "* * * * * /mnt/mmc/sonoff-hack/script/thumb.sh cron" >> /var/spool/cron/crontabs/root
+fi
 if [ "$FREE_SPACE" != "0" ]; then
     echo "0 * * * * sleep 20; /mnt/mmc/sonoff-hack/script/clean_records.sh $FREE_SPACE" >> /var/spool/cron/crontabs/root
 fi

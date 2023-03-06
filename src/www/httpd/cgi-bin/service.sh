f47efca440ecfb6b846119fce49510cc35352654
roleo <roleo@iperbole.bologna.it>
2023-03-06 13:52:33 +0100

    Fix onvif scope in service.sh

--- src/www/httpd/cgi-bin/service.sh
+++ src/www/httpd/cgi-bin/service.sh
@@ -96,7 +96,7 @@ start_onvif()
     echo "serial_num=$DEVICE_ID" >> $ONVIF_SRVD_CONF
     echo "ifs=$ONVIF_NETIF" >> $ONVIF_SRVD_CONF
     echo "port=$ONVIF_PORT" >> $ONVIF_SRVD_CONF
-    echo "scope=onvif://www.onvif.org/Profile/S" >> $ONVIF_SRVD_CONF
+    echo "scope=onvif://www.onvif.org/Profile/Streaming" >> $ONVIF_SRVD_CONF
     echo "" >> $ONVIF_SRVD_CONF
     if [ ! -z $ONVIF_PROFILE_0 ]; then
         echo "#Profile 0" >> $ONVIF_SRVD_CONF

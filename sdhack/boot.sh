5f22542b58d251bd3552e3811d1231c50e08719b
Florian Reinhard <florian.reinhard@googlemail.com>
2023-11-10 21:56:15 +0100

    Remove Audio Message if no WiFi is configured
    If WiFi is not configured there is another sound played.
    Probably every one using 200MP2B and this software has configured WiFi...

--- sdhack/boot.sh
+++ sdhack/boot.sh
@@ -10,6 +10,7 @@ mount --bind /var/sdcard/log /var/log
 touch /tmp/di.wav
 touch /tmp/Internet_connected_Welcome_to_use_cloud_camera.wav
 touch /tmp/WiFi_connect_success.wav
+touch /tmp/Please_use_mobile_phone_for_WiFi_configuration.wav
 
 WAV_FILE_DIR=/mnt/mtd/ipc/app/res/En
 [ -d /mnt/mtd/ipc/app/snd/english ] && WAV_FILE_DIR=/mnt/mtd/ipc/app/snd/english
@@ -17,6 +18,7 @@ WAV_FILE_DIR=/mnt/mtd/ipc/app/res/En
 mount --bind /tmp/di.wav $WAV_FILE_DIR/di.wav
 mount --bind /tmp/Internet_connected_Welcome_to_use_cloud_camera.wav $WAV_FILE_DIR/Internet_connected_Welcome_to_use_cloud_camera.wav
 mount --bind /tmp/WiFi_connect_success.wav $WAV_FILE_DIR/WiFi_connect_success.wav
+mount --bind /tmp/Please_use_mobile_phone_for_WiFi_configuration.wav $WAV_FILE_DIR/Please_use_mobile_phone_for_WiFi_configuration.wav
 
 # Add script for network management
 cp /mnt/mtd/ipc/app/script/dhcp.sh /tmp/dhcp.sh

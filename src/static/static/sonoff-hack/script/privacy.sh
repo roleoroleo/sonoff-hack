5458f91a8b9d349c4bc29dfbc0e0d01df8404b87
roleo <roleo@iperbole.bologna.it>
2024-04-20 16:00:04 +0200

    Fix switch on/off for cam where ProcessGuard is running - #168

--- src/static/static/sonoff-hack/script/privacy.sh
+++ src/static/static/sonoff-hack/script/privacy.sh
@@ -22,6 +22,16 @@ stop_alarmserver()
     ps | grep 'AlarmServer' | grep -v 'grep' | awk '{ printf $1 }' |xargs kill -SIGSTOP
 }
 
+start_ProcessGuard()
+{
+    ps | grep 'ProcessGuard' | grep -v 'grep' | awk '{ printf $1 }' |xargs kill -CONT 2> /dev/null
+}
+
+stop_ProcessGuard()
+{
+    ps | grep 'ProcessGuard' | grep -v 'grep' | awk '{ printf $1 }' |xargs kill -SIGSTOP 2> /dev/null
+}
+
 if [ $# -ne 1 ]; then
     exit
 fi
@@ -30,12 +40,14 @@ if [ "$1" == "on" ] || [ "$1" == "yes" ]; then
     if [ ! -f /tmp/privacy ]; then
         touch /tmp/privacy
         touch /tmp/snapshot.disabled
+        stop_ProcessGuard
         stop_rtsp
         stop_alarmserver
     fi
 elif [ "$1" == "off" ] || [ "$1" == "no" ]; then
     if [ -f /tmp/privacy ]; then
         rm -f /tmp/snapshot.disabled
+        start_ProcessGuard
         start_alarmserver
         start_rtsp
         rm -f /tmp/privacy

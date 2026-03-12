f70bd0dec5508e6403bae9e0c15c91cc3207be29
roleo <roleo@iperbole.bologna.it>
2026-03-12 15:03:03 +0100

    Fix error saving y position

--- src/ptz/ptz/ptz.c
+++ src/ptz/ptz/ptz.c
@@ -602,7 +602,7 @@ int main(int argc, char **argv)
         if (x < MIN_X) x = MIN_X;
         if (x > MAX_X) x = MAX_X;
         if (y < MIN_Y) y = MIN_Y;
-        if (y < MAX_Y) y = MAX_Y;
+        if (y > MAX_Y) y = MAX_Y;
 
         presets[0].x = x;
         presets[0].y = y;
